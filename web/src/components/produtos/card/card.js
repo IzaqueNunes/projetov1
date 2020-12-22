8125f151d2b218720f64b2618b17170e0886e489
IzaqueNunes <68032837+IzaqueNunes@users.noreply.github.com>
2020-12-22 15:49:04 -0300

    22/12
    Adicionado avaliação (falta setar valores para salva-los)
    
    Adicionado mapa da rota do queijo

--- web/src/components/produtos/card/card.js
+++ web/src/components/produtos/card/card.js
@@ -1,13 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './card.css';
-import {Link, Redirect} from "react-router-dom";
+import {Link} from "react-router-dom";
 import {BiTrash} from 'react-icons/bi';
 import UIButton from 'components/UI/Button/Button';
 import {isLogged} from 'components/utils/auth';
 import swal from 'sweetalert';
-import {Rating} from '@material-ui/lab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import ReactStars from "react-rating-stars-component";
+import { render } from "react-dom";
 
 import 'components/produtos/list/list.js';
 
@@ -24,37 +23,39 @@ const verproduto = (produtos)=> {
     ?onClickDelete() 
     :swal("Faça login para executar esta ação.")
 
+const ratingChanged = (newRating) =>{
+  console.log(newRating);
+  
+}
 
 
 const ProdutosCard = ({ produtos, onClickDelete }) => (
  
-  
+
   <div className="produtos-card">
     
     <img src={produtos.imageUrl} alt={produtos.title} className="produtos-card__image" />
     <div className="produtos-card__info">
       <h1 className="produtos-card__title" id="titulo">{produtos.title}</h1>
       <span className="produtos-card__price" >R$ {produtos.price}</span>
+      <br></br>
+      <span className='avaliacao'>Avaliação<ReactStars
+          count={5}
+          onChange={ratingChanged}
+          size={24}
+          isHalf={true}
+          emptyIcon={<i className="far fa-star"></i>}
+          halfIcon={<i className="fa fa-star-half-alt"></i>}
+          fullIcon={<i className="fa fa-star"></i>}
+          activeColor="#ffd700"
+          /></span>
       <footer className="produtos-card__footer">
-      {produtos.comments.length > 0 && (
-          <div className="produtos-card__comment">"{produtos.comments[0].comment}"</div>
-        )}
-        <button
-          className="produtos-card__comments-count">
-          {produtos.comments.length}{' '}
-          {produtos.comments.length > 1 ? 'Comentários' : 'Avaliações'}
-        </button>
+          
+      
+     
+     
+        
         <div className="btn-card">
-        <Box component="fieldset" mb={3} borderColor="transparent">
-        <Typography component="legend">Controlled</Typography>
-        <Rating
-          name="simple-controlled"
-          /* value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }} */
-        />
-      </Box>
         <UIButton
           component={Link}
           to={`/produto/${produtos.id}`}
@@ -66,6 +67,8 @@ const ProdutosCard = ({ produtos, onClickDelete }) => (
           to={`/edit/${produtos.id}`}
           className="btn-card__edit"
         >Editar</UIButton>
+        
+        
         </div>
       </footer>
       <button 
--- web/src/components/produtos/rotaqueijo/rotaqueijo.js
+++ web/src/components/produtos/rotaqueijo/rotaqueijo.js
@@ -1,156 +1,26 @@
-/* import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-
-class RotaQueijo extends Component {
- render(){
-    return (
-      <div>
-          <h1>Rota do Queijo</h1>
-            <Map google={this.props.google} 
-            style={{ height: "1000px", width: "1000px", position: "relatibve"}}
-            zoom={12}
-            google={this.props.google}
-            
-            initialCenter={{
-              lat:  -15.739797098828687,
-              lng: -43.022118122999956
-            }} 
-            onClick={this.onMapClicked}>
-
-                <Marker
-                    title={'Teste'}
-                    name={'IFNMG'}
-                    position={{ lat:  -15.739797098828687, lng: -43.022118122999956 }}
-                     />
-
-                <InfoWindow onClose={this.onInfoWindowClose}>
-                    
-                </InfoWindow>
-            </Map>
-      </div>
-    );
- }
-}
-export default GoogleApiWrapper({
-    apiKey: ("AIzaSyAyDlK8HVHUBDQTfV1RxCFzP-KLxmj3nZg")
-  })(RotaQueijo) */
-
- /*  import React from 'react'
-  import { GoogleMap, LoadScript } from '@react-google-maps/api';
-  
-  
-  function RotaQueijo() {
-    const [map, setMap] = React.useState(null)
-  
-    const onLoad = React.useCallback(function callback(map) {
-      const bounds = new window.google.maps.LatLngBounds();
-      map.fitBounds(bounds);
-      setMap(map)
-    }, [])
-  
-    const onUnmount = React.useCallback(function callback(map) {
-      setMap(null)
-    }, [])
-
-    const  containerStyle  = {
-        width :  ' 400px ' ,
-        height :  ' 400px '
-     };
-     
-     const  center  = {
-        lat:  -15.739797098828687,
-        lng: -43.022118122999956
-     };
-  
-    return (
-      <LoadScript
-        googleMapsApiKey="AIzaSyAyDlK8HVHUBDQTfV1RxCFzP-KLxmj3nZg"
-      >
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
-          onLoad={onLoad}
-          onUnmount={onUnmount}
-        >
-          { /* Child components, such as markers, info windows, etc.  }
-          <></>
-        </GoogleMap>
-      </LoadScript>
-    )
-  }
-  
-  export default React.memo(RotaQueijo) */
-  
 import React, {Component} from 'react';
-import {TileLayer, Marker, Popup, MapContainer} from 'react-leaflet';
-import 'leaflet/dist/leaflet-src';
-import './rotaqueijo.css';
-import L from 'leaflet';
-import styled from 'styled-components';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
-const position ={
-  lat:  -15.739797098828687,
-  lng: -43.022118122999956
-};
+const RotaQueijo = () => {
 
-const Wrapper = styled.div `
-  width: ${props => props.width};
-  height: ${props => props.height};
-  display: flex;
-  justify-content: center;
-  margin-top: 100px;
-  background-Color: red;
-`;
+return(
+  <div style={{ height: 800 }}>
+    <h1>Rota do Queijo em Porteirinha</h1>
+      <MapContainer style={{ height: 800 }} center={[-15.794600, -43.054010]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[-15.73955886649034, -43.02160109591189]}>
+          <Popup>
+            IFNMG <br /> Campus Porteirinha
+    </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+);
 
-
-/* 
-const RotaQueijo=()=> {
-
-     return (
-      <div className='map'>
-        <MapContainer style={Wrapper}
-        center={position} zoom={13} scrollWheelZoom={false}>
-           <TileLayer
-             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-           />
-           <Marker position={position}>
-           </Marker>
-        </MapContainer>
-      </div>
-     
-     );
 
 }
 
 export default RotaQueijo;
-
-
- */
-
- export default class RotaQueijo extends React.Component{
-
-  componentDidMount(){
-    this.map = L.map('map', {
-      center: [-15.739797098828687, -43.022118122999956],
-      position: [-15.739797098828687, -43.022118122999956],
-      zoom: 6,
-      zoomControl: false,
-    });
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      detectRetina: true,
-      maxZoom: 20,
-      maxNativeZoom: 17,
-    }).addTo(this.map);
-
-  }
-
-  render(){
-    return <Wrapper width='1280px' height= '720px' id='map'/>
-  }
-
-
- }
-
